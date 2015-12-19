dd826b0910df165ac034e121a530743a25f417b7
BenCo1967 <benco1967@gmail.com>
2015-12-19 14:30:50 +0100

    add drag& drop for horses creation
    The drag and drop off a list of names creates the corresponding horses

--- client/components/horse/horses.js
+++ client/components/horse/horses.js
@@ -35,4 +35,92 @@ angular.module('PlannerApp')
   $scope.cancelSave = function() {
     angular.copy(this.horseCopy, this.horse);
   };
+}])
+.directive('droppableHorsesName', ["mimeType", "$log", function(mimeType, $log) {
+  function setOverClass(el, e, scope) {
+    if(e.dataTransfer.types == "text/plain") {
+      el.classList.add('over-ok');
+    }
+  }
+  function resetOverClass(el) {
+    el.classList.remove('over-ok');
+  }
+  return {
+    scope: {
+      planner: "=",
+      lesson: "="
+    },
+    link: function(scope, element) {
+      // again we need the native object
+      var el = element[0];
+      
+      el.addEventListener(
+        'dragover',
+        function(e) {
+          e.dataTransfer.dropEffect = 'copy'; 
+          // allows us to drop
+          if (e.preventDefault) e.preventDefault();
+          setOverClass(this, e, scope);
+          return false;
+        },
+        false
+      );
+      
+      el.addEventListener(
+        'dragenter',
+        function(e) {
+          setOverClass(this, e, scope);
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragleave',
+        function(e) {
+          resetOverClass(this);
+          return false;
+        },
+        false
+      );
+      
+      el.addEventListener(
+        'drop',
+        function(e) {
+          // Stops some browsers from redirecting.
+          if (e.stopPropagation) e.stopPropagation();
+
+          resetOverClass(this);
+          var data = e.dataTransfer.getData('text/plain').split('\n');
+          for(var i = 0; i < data.length; i++) {
+            if(data[i]) {
+              Horses.insert({
+                name: data[i],
+                image: null,
+                creationDate: new Date()
+              });
+            }
+          }
+          return false;
+        },
+        false
+      );
+      
+      el.addEventListener(
+        'paste',
+        function(e) {
+          // Stops some browsers from redirecting.
+          if (e.stopPropagation) e.stopPropagation();
+
+          var data = e.clipboardData.getData('text/plain').split('\n');
+          for(var i = 0; i < data.length; i++) {
+            $log.info("drop " + data[i]);
+          }
+          
+          return false;
+        },
+        false
+      );
+    }
+  }
 }]);
