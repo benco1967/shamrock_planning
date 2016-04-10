e8da797c674b69de54d899582c3e0ea2d225c161
BenCo1967 <benco1967@gmail.com>
2016-04-10 23:07:10 +0200

    remove trace

--- models/users.js
+++ models/users.js
@@ -7,9 +7,6 @@ Meteor.users.allow({
     return true;
   },
   update: function (userId, user, fields, modifier) {
-    console.log(">>>>>> update user" + JSON.stringify(user));
-    console.log(">>>>>> update user admin " + Roles.userIsInRole(userId, "admin"));
-    console.log(">>>>>> update user himself" + (userId == user._id));
     return Roles.userIsInRole(userId, "admin") || userId == user._id;
   },
   remove: function (userId, user) {
